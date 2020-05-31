7f0cd01f7a7dfa2271509c3d09f974a495224824
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-30 21:14:36 -0300

    Create About page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from '../components/About/styled.js'
+
+export default function About() {
+  return (
+    <S.Container>
+      <h1>Hello BTC!</h1>
+    </S.Container>
+  )
+}
