7f65230bf987b1746131dc33f81d5b027c1013eb
Felipe Suri <felipeheredia1234@gmail.com>
2020-05-30 21:13:26 -0300

    Implemented Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,55 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `FelipeSuri`,
+    siteUrl: `https://btcsite.felipesuri.com`,
+    description: `Configurando o Gatsby`,
+    social: [
+      {
+        name: `Github`,
+        url: `https://github.com/felipesuri`,
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `FelipeSuri`,
+        short_name: `Suri`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#ffaa13`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#ffaa13`,
+        showSpinner: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-479479-10',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://btcsite.felipesuri.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
